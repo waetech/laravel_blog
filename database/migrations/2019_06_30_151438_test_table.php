cca5f266a0be021c1590bcc37ea3e5c55cde31ec
Roxanne Reyes <waetechsolutions@gmail.com>
2019-07-01 17:53:21 -0400

    Fixed database column, env file and mysql database name

--- database/migrations/2019_06_30_151438_test_table.php
+++ database/migrations/2019_06_30_151438_test_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class TestTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('test', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
-            $table->text('body');
-            $table->timestamps('created_at');
-            $table->timestamps('updated_at');
+            $table->timestamps();
         });
     }
 
@@ -29,6 +26,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('test');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@
 */
 
 Route::get('contact', 'PagesController@getContact');
-    
-
 Route::get('about', 'PagesController@getAbout');
-
 Route::get('/', 'PagesController@getIndex');
+Route::resource('posts', 'PostController');
