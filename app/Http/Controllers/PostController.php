77fd039e819884366fcc7e1b65614d6bb7ce9c35
Roxanne Reyes <waetechsolutions@gmail.com>
2019-07-04 10:33:18 -0400

    Updated form data to database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -85,7 +85,10 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        //find the post in the database and save it as a variable
+        $post = Post::find($id);
+        //return the view and pass in the var we previously created
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -97,7 +100,27 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //Validate the data
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ));
+
+        //Save the data to the database
+        $post = Post::find($id);
+
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        $post->save();
+
+
+        //set flash data with success message
+        Session::flash('success', 'This post successfully saved.');
+
+        //Redirect with flash data to posts.show
+        return redirect()->route('posts.show', $post->id);
+
     }
 
     /**
